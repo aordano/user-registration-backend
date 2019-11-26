eb355c72e420cd781d64ee041e31a192d69d18aa
Agata Irene Ordano <aordano@protonmail.com>
2019-11-26 17:00:11 -0300

    feat: added FS operation for creating data on POST request
    Also redirection and YAML input sanitizing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs'); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs'); 
+var endOfLine = require('os').EOL;
 
-/* GET home page. */
-router.post('/', function(req, res, next) {
-    res.send('Got a POST request');
+/* POST home page. */
+router.post('/', function (req, res) {
+    var email = req.body.email;
+    var name = req.body.name;
+    var surname = req.body.surname;
+    var organization = req.body.organization;
+    var position = req.body.position;
+    var composed_text = req.body.composed_text;
+    var subscribe = req.body.subscribe;
+
+
+    var filecontent =
+        'email: ' + String(email) + endOfLine +
+        'name: ' + String(name).replace(/:/g, '') + endOfLine +
+        'surname: ' + String(surname).replace(/:/g, '') + endOfLine +
+        'organization: ' + String(organization).replace(/:/g, '') + endOfLine +
+        'position: ' + String(position).replace(/:/g, '') + endOfLine +
+        '---' + endOfLine +
+        String(composed_text);
+
+    var pathname =
+        "/var/www/contact-review/content/" +
+        String(Date.now()) +
+        String(organization).split(' ')[0] + '/';
+
+    var fullpathname = pathname + "index.md";
+
+    console.log(pathname);
+    fs.mkdir(pathname, err => {
+        if (err && err.code != 'EEXIST') throw 'error; most likely path is broken somewhere'
+        fs.writeFile(fullpathname, filecontent, (err) => {
+            if (err) throw err;
+        });
+
+        res.redirect('https://nodoambiental.org/contactsuccess.html')
+
+    });
+
 });
 
 module.exports = router;
