5b4abad3f75a65b79d61e078a9999fac720d5586
Ágata Irene Ordano <aordano@protonmail.com>
2020-10-08 12:44:36 -0300

    cleanup: minor fix in docs and cleaned up the node commands

--- execute.sh
+++ execute.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-echo ' ~~~~~ Membership/Leads Backend ~~~~~ '
-echo ''
-echo 'Microservice to handle the reception and manipulation of leads or potential members data'
-echo ''
-rm -rf ./build/*
-mkdir ./build/tables
-cp -R ./tables/* ./build/tables/
-echo 'Previous compilation cleaned.'
-tsc
-echo 'Typescript compiled. Generating docs...'
-typedoc 
-cp -R ./docsDependencies/* ./docs/assets/js/
-echo ''
-echo 'Server launched on port 9898.'
+bash ./build.sh
+echo 'Server launching on port 9898.'
 echo 'Now listening.'
 echo ''
 node ./start.js
